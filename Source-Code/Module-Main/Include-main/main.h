08c7a1015d0a356a90486ec8b477f23f6cae802d
tea.t <souri@kth.se>
2019-03-28 11:36:24 +0100

    More useless stuff...

---
+++ Source-Code/Module-Main/Include-main/main.h
@@ -0,0 +1,11 @@
+/* Well I was supposed to do write some c-code but I dont't
+feel like it right now.*/
+
+#Include "ranta.h"
+
+#if !defined(main.h)
+  #define main_h
+
+  const double lala = 1 + 1;
+
+#endif
